e7a13cf398b5fcd5add94a5abd859c0eded27819
Thomas Mahler <thma@apache.org>
2018-01-28 22:35:50 +0100

    clean up

--- app/Main.hs
+++ app/Main.hs
@@ -11,37 +11,40 @@ import           System.Directory              (doesFileExist)
 import           Text.Blaze.Html.Renderer.Text (renderHtml)
 import           Text.Markdown                 (def, markdown)
 import           Yesod
-import           Yesod.Default.Config
 
 data HsWiki = HsWiki
 
 mkYesod "HsWiki" [parseRoutes|
 /           HomeR   GET
-/#Text      OtherR  GET
+/#Text      PageR   GET
 /edit/#Text EditR   GET POST
 |]
 
 instance Yesod HsWiki
 
+main :: IO ()
+main = warp 3000 HsWiki
+
+-- Route Handlers
+getHomeR :: Handler Html
+getHomeR = getPageR "index"
+
+getPageR :: Text -> Handler Html
+getPageR page = do
+    let fileName = fileNameFor page
+    exists <- liftIO $ doesFileExist fileName
+    if exists then do
+        content <- liftIO $ readFile fileName
+        return $ htmlFromMd page content
+    else do
+        let content = newPage
+        liftIO $ writeFile fileName content
+        redirect $ EditR page
+
 getEditR :: Text -> Handler TypedContent
 getEditR page = do
     editor <- liftIO $ editorFor page
     sendResponse $ TypedContent typeHtml $ toContent editor
-
-
-editorFor :: Text -> IO String
-editorFor page = do
-    let fileName = fileNameFor page
-    md <- liftIO $ readFile fileName
-    return $ "<form action=\"" ++ unpack page ++ "\" method=\"POST\">" ++
-                "<textarea name=\"content\" cols=\"120\" rows=\"40\">" ++
-                md ++
-                "</textarea><button>save</button></form>"
-
-fileNameFor :: Text -> String
-fileNameFor page =
-    --conf <- loadloadDevelopmentConfig
-    "content/" ++ unpack page ++ ".md"
 
 postEditR :: Text -> Handler Html
 postEditR page = do
@@ -49,34 +52,33 @@ postEditR page = do
     maybeContent <- lookupPostParam "content"
     case maybeContent of
         Just content -> liftIO $ writeFile fileName $ T.unpack content
-    redirect $ OtherR page
+    redirect $ PageR page
 
-getHomeR :: Handler Html
-getHomeR = getOtherR $ pack "index"
-
-getOtherR :: Text -> Handler Html
-getOtherR page = do
-    let pageName = unpack page
+-- helper functions
+editorFor :: Text -> IO String
+editorFor page = do
     let fileName = fileNameFor page
-    exists <- liftIO $ doesFileExist fileName
-    if exists then do
-        content <- liftIO $ readFile fileName
-        return $ fullPage pageName content
-    else do
-        let content = newPage
-        liftIO $ writeFile fileName content
-        return $ fullPage pageName content
+    md <- liftIO $ readFile fileName
+    return $ htmlHeader page ++
+        "<form action=\"" ++ unpack page ++ "\" method=\"POST\">" ++
+            "<textarea name=\"content\" cols=\"120\" rows=\"30\">" ++
+            md ++
+            "</textarea><button>save</button></form>"
+
+fileNameFor :: Text -> String
+fileNameFor page =
+    "content/" ++ unpack page ++ ".md"
 
 newPage :: String
-newPage = "\r\nUse MarkDown to format page content"
+newPage = "\r\n## Use MarkDown to format page content"
 
 pageHeader :: String -> String
 pageHeader page =
-        "[home](index) | versions | referenced by | [edit](edit/" ++ page ++ ") | \r\n\r\n" ++
+        "[home](/) | versions | referenced by | [edit](/edit/" ++ page ++ ") | \r\n\r\n" ++
         "#" ++ page ++ "\r\n"
 
-fullPage :: String -> String -> Html
-fullPage page content = markdown def $ pack $ pageHeader page ++ content
+htmlHeader :: Text -> String
+htmlHeader page = unpack $ renderHtml $ markdown def $ pack $ pageHeader (unpack page)
 
-main :: IO ()
-main = warp 3000 HsWiki
+htmlFromMd :: Text -> String -> Html
+htmlFromMd page content = markdown def $ pack $ pageHeader (unpack page) ++ content
